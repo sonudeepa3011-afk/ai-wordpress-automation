fce9743735a79bcccd6c50e38834a7f8e12a5431
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-23 19:03:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,34 @@ response = requests.get(first_post, headers={"User-Agent":"Mozilla/5.0"})
 soup = BeautifulSoup(response.text, "html.parser")
 
 content = soup.find("article")
+from google import genai
+import os
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+prompt = f"""
+Rewrite this article completely.
+
+Rules:
+- 100% unique
+- SEO Friendly
+- Human written
+- Keep facts same
+- Create new title
+- Create headings
+- No plagiarism
+
+Article:
+{content.get_text(" ", strip=True)}
+"""
+
+response = client.models.generate_content(
+    model="gemini-flash-latest",
+    contents=prompt
+)
+
+print("\n========== AI ARTICLE ==========\n")
+print(response.text[:5000])
 
 if content:
     print(content.get_text(" ", strip=True)[:3000])
