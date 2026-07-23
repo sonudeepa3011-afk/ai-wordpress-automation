c0554bcd9855248de268fc7f803b5b5361761941
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-23 18:40:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+from google import genai
 
-print("AI WordPress Automation Started...")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-api_key = os.getenv("GEMINI_API_KEY")
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Write a 50-word news about AI in simple English."
+)
 
-if api_key:
-    print("✅ Gemini API Key Found")
-else:
-    print("❌ Gemini API Key Not Found")
+print(response.text)
