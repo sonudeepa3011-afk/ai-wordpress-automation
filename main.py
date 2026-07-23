8e43da60dde2528373d7e301dde80a92e7279833
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-23 21:42:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from modules.rss import get_latest_posts
+from modules.scraper import get_article_content
 
 RSS_FEEDS = [
     "https://www.karmasandhan.com/feed/",
 ]
 
 for feed in RSS_FEEDS:
-    print(f"\nReading Feed: {feed}\n")
 
-    posts = get_latest_posts(feed)
+    posts = get_latest_posts(feed, limit=1)
 
     for post in posts:
+
         print("TITLE :", post["title"])
         print("LINK  :", post["link"])
-        print("DATE  :", post["published"])
-        print("-" * 60)
+
+        print("\nFetching Article...\n")
+
+        content = get_article_content(post["link"])
+
+        print(content[:3000])
