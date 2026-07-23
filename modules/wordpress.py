f55b721976c911a164c8f6ddac9daba42587894f
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-23 22:20:26 +0530

    Update wordpress.py

--- modules/wordpress.py
+++ modules/wordpress.py
@@ -1,7 +1,6 @@
 import os
 import requests
 
-
 def create_draft(title, content):
 
     wp_url = os.getenv("WP_URL")
@@ -19,6 +18,14 @@ def create_draft(title, content):
         auth=(wp_user, wp_pass),
         json=data,
         timeout=30
+    )
+
+    print("Status Code:", response.status_code)
+    print(response.text)
+
+    response.raise_for_status()
+
+    return response.json()
     )
 
     response.raise_for_status()
