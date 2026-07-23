4d0bd0ec42f04e00172aeb1a151f618d56832315
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-23 18:43:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash",
+    model="gemini-flash-latest",
     contents="Write a 50-word news about AI in simple English."
 )
 
