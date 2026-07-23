0a5bd6e2c37e0ed0ec887f9dad45c1a8ea17177b
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-23 18:28:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import os
+
 print("AI WordPress Automation Started...")
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+if api_key:
+    print("✅ Gemini API Key Found")
+else:
+    print("❌ Gemini API Key Not Found")
