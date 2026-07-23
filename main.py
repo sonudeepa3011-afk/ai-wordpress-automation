72936222865b14593700e7b7446897d49e35c71a
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-23 21:57:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from modules.rss import get_latest_posts
+from modules.scraper import get_article_content
+from modules.ai import rewrite_article
+from modules.rss import get_latest_posts
 
 RSS_FEEDS = [
     "https://www.karmasandhan.com/feed/",
