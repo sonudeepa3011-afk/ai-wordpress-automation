f606d6f1f6758500ef4529c56eecf60c0166a801
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-23 18:58:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ for post in feed.entries[:10]:
     print("TITLE :", post.title)
     print("LINK  :", post.link)
     print("-" * 50)
+import requests
+from bs4 import BeautifulSoup
+
+first_post = feed.entries[0].link
+
+print("\nOpening:", first_post)
+
+response = requests.get(first_post, headers={"User-Agent":"Mozilla/5.0"})
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+content = soup.find("article")
+
+if content:
+    print(content.get_text(" ", strip=True)[:3000])
+else:
+    print("Content Not Found")
